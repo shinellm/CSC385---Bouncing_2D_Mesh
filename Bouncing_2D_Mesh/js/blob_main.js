50ac0113052a9ea45600db61f1cc1554c77db3fb
Shinell Manwaring <shinell.manwaring@gmail.com>
2018-03-11 16:14:38 -0400

    Deleted the bouncing ball test and removed commented out lines from blob_main.

--- Bouncing_2D_Mesh/js/blob_main.js
+++ Bouncing_2D_Mesh/js/blob_main.js
@@ -24,34 +24,13 @@ var mouse = {x:0, y:0};
 var gravity = 0.5;
 var bounce_factor = 0.8;
 
-// A test blob
-/*
-var blob = {
-    pos: {x: WIDTH/2, y: HEIGHT/3},
-    velocity: {x: 0, y: 0},
-    mass: 0.1, //kg
-    rad: 15 // 1px = 1cm
-};
-*/
-
-//var Cd = 0.47;  // Dimensionless
-//var rho = 1.22; // kg / m^3
-//var A = Math.PI * blob.rad * blob.rad / (10000); // m^2
-//var ag = 9.81;  // m / s^2
-
-
 function init(){
 
     // Initialize WebGL.
     canvas = document.getElementById("gl-canvas");
+    canvas.onclick = getMousePosition;
 
     gl = WebGLUtils.setupWebGL(canvas);
-
-    //ctx = canvas.getContext("2d"); //Rendering in 2D
-    //canvas.onclick = getMousePosition;
-    //ctx.fillStyle = 'blue'; //Sets the filled color for the blob
-    //ctx.strokeStyle = '#000000'; //Sets the outline color for the blob
-    //loopTimer = setInterval(loop, frameDelay);
 
     if (!gl){
         alert("WebGL isn't available");
@@ -79,11 +58,6 @@ function getMousePosition(event) {
     mouse.x = event.clientX - canvas.offsetLeft; //Get the x-coordinate of the mouse
     mouse.y = event.clientY - canvas.offsetTop; //Get the y-coordinate of the mouse
 
-    /*blob.pos.x = mouse.x; //Set mouse.x as the blob's x-coordinate
-    blob.pos.y = mouse.y; //Set mouse.y as the blob's y-coordinate
-    blob.velocity.x = 0; //Reset the blob's velocity.x
-    blob.velocity.y = 0; //Reset the blob's velocity.y*/
-
     blob.center.pos.x = mouse.x; //Set mouse.x as the blob's x-coordinate
     blob.center.pos.y = mouse.y; //Set mouse.y as the blob's y-coordinate
     blob.center.velocity.x = 0 //Reset the blob's velocity.x
@@ -96,12 +70,6 @@ function getMousePosition(event) {
 
 
 function loop() {
-    /*
-    blob.velocity.y += gravity; //Set the blob's new velocity.y
-
-    blob.pos.x += blob.velocity.x; //Set the blob's new x-coordinate
-    blob.pos.y += blob.velocity.y; //Set the blob's new y-coordinate
-    */
 
     var blob = blob_world.get_blob();
     blob.center.velocity.y += gravity; //Set the blob's new velocity.y
@@ -131,30 +99,14 @@ function loop() {
         ball.position.y += ball.velocity.y*frameRate*100;
     }*/
 
-
     // Handle collisions with the perimeter of the canvas
-    /*if (blob.pos.y > HEIGHT - blob.rad || blob.pos.x > WIDTH - blob.rad || blob.pos.x < blob.rad) {
-        blob.pos.y = HEIGHT - blob.rad;
+    if (blob.center.pos.y > HEIGHT - blob.rad || blob.center.pos.x > WIDTH - blob.rad || blob.center.pos.x < blob.rad) {
+        blob.center.pos.y = HEIGHT - blob.rad;
         //blob.pos.x = WIDTH/2;
 
-        blob.velocity.x = 0; //Set the blob's velocity x
-        blob.velocity.y *= -bounce_factor; //Set the blob's velocity y
+        blob.center.velocity.x = 0; //Set the blob's velocity x
+        blob.center.velocity.y *= -bounce_factor; //Set the blob's velocity y
     }
-    */
-
-    /*
-    // Draw the test blob
-    ctx.clearRect(0,0,WIDTH,HEIGHT);
-    ctx.save();
-
-    ctx.translate(blob.center.pos.x, blob.center.pos.y);
-    ctx.beginPath();
-    ctx.arc(0, 0, blob.rad, 0, Math.PI*2, true); //Create the blob using arcs
-    ctx.fill(); //Fill the blob
-    //ctx.stroke(); //Outline the blob
-    ctx.closePath();
-
-    ctx.restore();
-    */
+    
 
 }
