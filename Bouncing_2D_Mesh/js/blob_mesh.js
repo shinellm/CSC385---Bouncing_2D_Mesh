f587e961ec62658458deaa762741c55a3ef5e1aa
Shinell Manwaring <shinell.manwaring@gmail.com>
2018-03-11 19:39:02 -0400

    More detailed comments to blob_mesh.js.

--- Bouncing_2D_Mesh/js/blob_mesh.js
+++ Bouncing_2D_Mesh/js/blob_mesh.js
@@ -1,5 +1,10 @@
 class Point {
 
+    /**
+     * Creates a Point from a specified position.
+     * @param pos {vec4}
+     *      The position of the Point
+     */
     constructor(pos){
 
         this.pos = pos;  //When setting new pos values, be sure to include 1 as w-value, always.
@@ -10,12 +15,15 @@ class Point {
 
     }
 
-    /*
+    /**
      * For setting position and color of new odd vertices or old even vertices,
      * based on surrounding ones. For parts b. and d. of Loop formula.
-     * param: verts the surrounding vertices used as basis for scaling
-     * param: weights the weights with which to scale each surrounding vertex's color and position
-     * param: s the scalar of this Vertex, to be done before adding its neighbors' weights
+     * @param: verts
+     *      The surrounding vertices used as basis for scaling
+     * @param: weights
+     *      The weights with which to scale each surrounding vertex's color and position
+     * @param: s
+     *      The scalar of this Vertex, to be done before adding its neighbors' weights
      * NOTE: want to scale each even vertex by its neighbors' values, as they all
      * were before subdivision, so verts here must be a copy of these values, not
      * the actual Vertex objects of the Mesh.
@@ -37,16 +45,19 @@ class Point {
 
 class Blob {
 
-    /*
+    /**
      * Forms a blob from a specified
      * center, a radius, and the number
      * of point masses on its perimeter
-     * @param center {vec4} the center of the blob
-     * @param rad {num} this blob's radius, when the
-     * blob is represented as a circle, before any
-     * forces act upon it
-     * @param num_points {num} the number of points
-     * on the perimeter of this blob
+     * @param center {vec4}
+     *      The center of the blob
+     * @param rad {num}
+     *      This blob's radius, when the blob is
+     *      represented as a circle, before any
+     *      forces act upon it
+     * @param num_points {num}
+     *      The number of points on the perimeter
+     *      of this blob
      */
     constructor(center, rad, num_points){
         this.center = new Point(center);
@@ -91,17 +102,19 @@ class Blob {
         //this.Bezier();
     }
 
-    /*
+    /**
      * Calculates the positions of
      * the control points of the control
      * cage for the current portion of this
      * Blob's cubic curve.
-     * @param p0 {vec4} the "P0" of the current
-     * control cage
-     * @param p3 {vec4} the "P3"
-     * @return [pos1, pos2] the second and third
-     * positions of the second and third points
-     * on the cage.
+     *
+     * @param p0 {vec4}
+     *      The "P0" of the current control cage
+     * @param p3 {vec4}
+     *      The "P3"
+     * @return [pos1, pos2]
+     *      The second and third positions of the
+     *      second and third points on the cage.
      */
     calculate_controls(p0, p3) {
 
@@ -116,32 +129,7 @@ class Blob {
         return [pos1, pos2];
     }
 
-    /*
-     * Calculates the positions of
-     * the control points of the control
-     * cage for the current portion of this
-     * Blob's cubic curve.
-     * @param p0 {vec4} the "P0" of the current
-     * control cage
-     * @param p3 {vec4} the "P3"
-     * @return [pos1, pos2] the second and third
-     * positions of the second and third points
-     * on the cage.
-     */
-    calculate_controls(p0, p3) {
-
-        var comp1 = subtract(p0.pos, p0.right_neighbor.pos);
-        var vect1 = normalize(add(subtract(p0.pos, p0.right_neighbor.pos), subtract(p0.left_neighbor.pos, p0.pos)));
-        var pos1 = add(p0.pos,scale(-dot(vect1, comp1)/dot(vect1,vect1),vect1));
-
-        var comp2 = subtract(p3.pos, p3.left_neighbor.pos);
-        var vect2 = normalize(add(subtract(p3.pos, p3.left_neighbor.pos), subtract(p3.right_neighbor.pos, p3.pos)));
-        var pos2 = add(p3.pos, scale(-dot(vect2,comp2)/dot(vect2,vect2), vect2));
-
-        return [pos1, pos2];
-    }
-
-    /*
+    /**
      * Recursively creates new, smaller
      * control cages to add new positions
      * along this Blob's cubic curve.
@@ -151,15 +139,16 @@ class Blob {
      * to each other, at which point we apply
      * the Bezier geometric matrix to find
      * the final points along the curve
-     * @param {p0, p1, p2, p3} {vec4}: the
-     * control points of the control cage for
-     * this Blob's cubic Bezier curve
+     *
+     * @param {p0, p1, p2, p3} {vec4}
+     *      The control points of the control
+     *      cage for this Blob's cubic Bezier curve.
      */
     deCasteljau(p0, p1, p2, p3) {
 
     }
 
-    /*
+    /**
      * Creates smooth arcs between trios
      * of this Blob's outer points.
      */
@@ -199,13 +188,15 @@ class Blob {
 
 
 class BlobWorld {
-    /*
+    /**
      * The world in which the Blob exists.
      * Includes the blob and the pixel array
      * from the program, that actually colors
      * the appropriate pixels, based on the
      * locations of the point masses of the blob.
-     * @param {Blob} the Blob this world contains
+     *
+     * @param {Blob}
+     *      The Blob this world contains.
      */
     constructor(blob, gl, program) {
         this.blob = blob;
@@ -223,6 +214,13 @@ class BlobWorld {
         return this.blob;
     }
 
+    /**
+     * Sets every point on the blob to a new position
+     * based on gravity.
+     *
+     * @param {number} gravity
+     *      The gravity applied to the blob.
+     */
     free_fall(gravity){
 
         this.blob.center.velocity[1] -= gravity; //Set the new velocity.y of the blob's center
@@ -236,6 +234,14 @@ class BlobWorld {
         }
     }
 
+    /**
+     * Sets every point on the blob to a new position
+     * based on the position of the mouse click on
+     * the canvas.
+     *
+     * @param {object} mouse
+     *      The position of the mouse click on the canvas.
+     */
     new_position(mouse){
         var mousex = mouse.x; //x-coordinate of the mouse click
         var mousey = mouse.y; //y-coordinate of the mouse click
@@ -261,6 +267,15 @@ class BlobWorld {
         }
     }
 
+    /**
+     * Evolves the blob by moving every point on the
+     * blob and checks for collisions with the canvas.
+     *
+     * @param {number} h
+     *      The height of the canvas.
+     * @param {number} w
+     *      The width of the canvas.
+     */
     evolve(h, w){
         var height = h;
         var width = w;
